1263b0bee130422e9dfdaf8347aa6176b70ed2bb
Chris McGinn <chris.s.mcginn@live.com>
2013-11-21 06:31:45 -0500

    Added persisting of state
    Using application shared preferences save state of checked item, create
    unit test, passes

--- Testing/ApplicationTesterTest/src/com/example/applicationundertest/test/MainActivityTest.java
+++ Testing/ApplicationTesterTest/src/com/example/applicationundertest/test/MainActivityTest.java
@@ -1,6 +1,10 @@
 package com.example.applicationundertest.test;
 
+import android.annotation.SuppressLint;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -8,10 +12,11 @@ import com.example.applicationundertest.MainActivity;
 
 public class MainActivityTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
-	
+
 	private MainActivity mActivity;
 	private TextView mWelcomeText;
 	private ListView mListView;
+
 	@SuppressWarnings("deprecation")
 	public MainActivityTest() {
 		super("com.example.applicationundertest", MainActivity.class);
@@ -21,25 +26,48 @@ public class MainActivityTest extends
 	protected void setUp() throws Exception {
 		super.setUp();
 		setActivityInitialTouchMode(false);
-		
+
 		mActivity = getActivity();
-		mWelcomeText = (TextView)mActivity.findViewById(com.example.applicationundertest.R.id.welcome_text);
+		mWelcomeText = (TextView) mActivity
+				.findViewById(com.example.applicationundertest.R.id.welcome_text);
 		mListView = mActivity.getListView();
 
 	}
-	
-	public void testPreConditions(){
-		
+
+	public void testPreConditions() {
+
 		assertTrue(mWelcomeText.getText().equals("Hello world!"));
 		assertTrue(mListView.getOnItemClickListener() != null);
 	}
-	//Runs on UI thread can be used to select something
-	public void testWelcomeText(){
-		mActivity.runOnUiThread(new Runnable(){
-			public void run(){
-				mWelcomeText.requestFocus();
+
+	// Runs on UI thread can be used to select something
+	@SuppressLint("NewApi")
+	public void testWelcomeText() {
+
+		mActivity.runOnUiThread(new Runnable() {
+
+			@SuppressLint("NewApi")
+			public void run() {
 				
+				mListView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+				mListView.setItemChecked(2, true);		
+				assertTrue(mListView.getCheckedItemCount() > 0);
 			}
 		});
+
+	}
+	@SuppressLint("NewApi")
+	@UiThreadTest
+	public void testStatePause(){
+		mListView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+		mListView.setItemChecked(2, true);		
+		assertTrue(mListView.getCheckedItemCount() > 0);
+		Instrumentation mInstr = this.getInstrumentation();
+		mInstr.callActivityOnPause(mActivity);
+		//clear all checked
+		mListView.clearChoices();
+		assertTrue(mListView.getCheckedItemCount() == 0);
+		mInstr.callActivityOnResume(mActivity);
+		assertTrue(mListView.getCheckedItemCount() > 0);
 	}
 }
--- Testing/ApplicationUnderTest/src/com/example/applicationundertest/MainActivity.java
+++ Testing/ApplicationUnderTest/src/com/example/applicationundertest/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.applicationundertest;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
+	public static final String PREFERENCES_FILE = "MainPrefs";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +36,65 @@ public class MainActivity extends ListActivity {
     public void onListItemClick(ListView l, View v, int position, long id){
     	
     }
-    
+    @Override
+    public void onPause(){
+    	super.onPause();
+        if (!writeInstanceState(this)) {
+            Toast.makeText(this,
+                    "Failed to write state!", Toast.LENGTH_LONG).show();
+         }
+    }
+    @Override
+    public void onResume(){
+    	super.onResume();
+    	readInstanceState(this);
+    }
+    @SuppressWarnings("unused")
+	public boolean readInstanceState(Context c){
+    	boolean result = false;
+        /*
+         * The preferences are stored in a SharedPreferences file. The abstract implementation of
+         * SharedPreferences is a "file" containing a hashmap. All instances of an application
+         * share the same instance of this file, which means that all instances of an application
+         * share the same preference settings.
+         */
+
+        /*
+         * Get the SharedPreferences object for this application
+         */
+
+        SharedPreferences p = c.getSharedPreferences(PREFERENCES_FILE, MODE_WORLD_READABLE);
+        int itemPos = p.getInt("MyItemPosition", -1);
+        ListView listView = getListView();
+        listView.setItemChecked(itemPos, true);
+        result =  (p.contains("MyItemPosition"));
+    	return result;
+    	
+    }
+    @SuppressWarnings("unused")
+	public boolean writeInstanceState(Context c){
+    	boolean result =false;
+        /*
+         * Get the SharedPreferences object for this application
+         */
+
+        @SuppressWarnings("deprecation")
+		SharedPreferences p =
+                c.getSharedPreferences(MainActivity.PREFERENCES_FILE, MODE_WORLD_READABLE);
+        
+        /*
+         * Get the editor for this object. The editor interface abstracts the implementation of
+         * updating the SharedPreferences object.
+         */
+
+        SharedPreferences.Editor e = p.edit();
+        /*
+         * Write the keys and values to the Editor
+         */
+        ListView listView = getListView();
+        int itemPos = listView.getCheckedItemPosition();
+        e.putInt("MyItemPosition", itemPos);
+        result = e.commit();
+    	return result;
+    }
 }
